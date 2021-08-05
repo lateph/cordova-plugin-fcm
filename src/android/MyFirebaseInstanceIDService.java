1043849774667f903fde7c56b66da09150c4dad7
Lateph Herlino <lateph@gmail.com>
2021-08-05 20:04:05 +0700

    Update MyFirebaseInstanceIDService.java
    update

--- src/android/MyFirebaseInstanceIDService.java
+++ src/android/MyFirebaseInstanceIDService.java
@@ -2,24 +2,18 @@ package com.gae.scaffolder.plugin;
 
 import android.util.Log;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
 /**
  * Created by Felipe Echanique on 08/06/2016.
  */
-public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+public class MyFirebaseInstanceIDService extends FirebaseMessagingService {
 
     private static final String TAG = "FCMPlugin";
 
     @Override
-    public void onTokenRefresh(){
-        // Get updated InstanceID token.
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
-		FCMPlugin.sendTokenRefresh( refreshedToken );
-
-        // TODO: Implement this method to send any registration to your app's servers.
-        //sendRegistrationToServer(refreshedToken);
+    public void onNewToken(String token){
+      FCMPlugin.sendTokenRefresh( token );
+      Log.d(TAG, "Refreshed token: " + token);
     }
 }
